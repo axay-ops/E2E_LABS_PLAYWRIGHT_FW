0fd423f33460bc37ad54aace01b1c0758b1a77f6
AXAY <cmsqa.automation@gmail.com>
2026-02-05 19:30:11 -0600

    added context close in base fixture

--- fixtures/baseFixtures.ts
+++ fixtures/baseFixtures.ts
@@ -17,7 +17,7 @@ export const test = base.extend<customFixtures>({
      let homePage = await loginPage.doLogin(testInfo.project.metadata.appAdminUsername, testInfo.project.metadata.appAdminPassword); 
      expect(await homePage.isUserLoggedIn()).toBeTruthy(); 
      await use(homePage);
-     // await context.close();
+     await context.close();
      // attach videos manually, since context is created manually (not by PW's page)
      // const videoPath = await mypage.video()?.path();
     //  console.log(`Video saved in standard location: ${videoPath}`)
@@ -37,7 +37,7 @@ export const test = base.extend<customFixtures>({
      let homePage = await loginPage.doLogin(testInfo.project.metadata.appCustomerUsername, testInfo.project.metadata.appCustomerPassword); 
      expect(await homePage.isUserLoggedIn()).toBeTruthy(); 
      await use(homePage);
-     // await context.close();
+     await context.close();
     }
 });
 
